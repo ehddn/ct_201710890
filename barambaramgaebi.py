0a594a2d142f92bbdedb297b18ba285224ca2287
ehddn <ehddn2202@naver.com>
2017-05-09 10:47:49 +0900

    Add files via upload

---
+++ barambaramgaebi.py
@@ -0,0 +1,14 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+def baram(size):
+    for i in range(1,9,1):
+        t1.fd(size)
+        t1.rt(90)
+        t1.fd(size)
+        t1.penup()
+        t1.home()
+        t1.setheading(45*i)
+        t1.pendown()
+baram(100)
+   
